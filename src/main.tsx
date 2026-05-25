85513264de4708cb1fd813ad8d8f2cdf9aad7fa1
Rafik <rafik@example.com>
2026-05-26 00:43:09 +0100

    correcione 2

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,12 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-console.log('React starting...');
-
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  console.error('Failed to find the root element');
-} else {
+
+if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
       <App />
     </StrictMode>,
   );
-  console.log('React render called');
 }
-
